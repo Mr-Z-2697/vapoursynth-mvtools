97a01fdb46804d68c7cc72de73c4beb9c77ea600
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2026-03-12 01:56:45 +0800

    Fix DepanAnalyse crash

--- src/MVDepan.cpp
+++ src/MVDepan.cpp
@@ -599,7 +599,7 @@ static void VS_CC depanAnalyseCreate(const VSMap *in, VSMap *out, void *userData
         {data->mask, rpStrictSpatial},
     };
 
-    vsapi->createVideoFilter(out, "DepanAnalyse", data->vi, depanAnalyseGetFrame, depanAnalyseFree, fmParallel, deps, ARRAY_SIZE(deps), data, core);
+    vsapi->createVideoFilter(out, "DepanAnalyse", data->vi, depanAnalyseGetFrame, depanAnalyseFree, fmParallel, deps, ARRAY_SIZE(deps) - !data->mask /*mask may not exist*/, data, core);
 
     if (vsapi->mapGetError(out)) {
         depanAnalyseFree(data, core, vsapi);
